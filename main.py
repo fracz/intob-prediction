7734a9751cb3b5519a9128d3a1a02443951905c4
Jakub Tyrcha <jakub.tyrcha@gmail.com>
2013-10-21 12:32:21 +0200

    kursy euro
    pobieranie danych

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print 'sprawdzam czy umiem obslugiwac gita'
+from pybrain.tools.shortcuts import buildNetwork
+
+net = buildNetwork(2, 3, 1)
+
+def find(l, e):
+	return next(i for i in xrange(len(l)) if l[i] == e)
+
+# zwraca kursy z kolejnych dni jako tuple wartosci(w dolarach, jenach?, funtach)
+def parseEuroXRef(file):
+	f = open(file, 'r')
+	header = f.readline().split(',')
+
+	rates = []
+
+	usdIndex = find(header, 'USD')
+	jpyIndex = find(header, 'JPY')
+	gbpIndex = find(header, 'GBP')
+
+	for line in f:
+		split = line.split(',')
+		r = float(split[usdIndex]), float(split[jpyIndex]), float(split[gbpIndex])
+		rates.append(r)
+
+	f.close()
+	rates.reverse()
+	return rates
+
+rates = parseEuroXRef('trainingdata/eurofxref-hist.csv')
+print rates

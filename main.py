e7b7dcbe50d6f3cf3dfaa1bc572b7ddac171279c
Jakub Tyrcha <jakub.tyrcha@gmail.com>
2013-10-21 16:43:22 +0200

    trenowanie sieci

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from pybrain.tools.shortcuts import buildNetwork
-
-net = buildNetwork(2, 3, 1)
-
 def find(l, e):
 	return next(i for i in xrange(len(l)) if l[i] == e)
 
-# zwraca kursy z kolejnych dni jako tuple wartosci(w dolarach, jenach?, funtach)
+# zwraca kursy z kolejnych dni
 def parseEuroXRef(file):
 	f = open(file, 'r')
 	header = f.readline().split(',')
@@ -13,12 +9,10 @@ def parseEuroXRef(file):
 	rates = []
 
 	usdIndex = find(header, 'USD')
-	jpyIndex = find(header, 'JPY')
-	gbpIndex = find(header, 'GBP')
 
 	for line in f:
 		split = line.split(',')
-		r = float(split[usdIndex]), float(split[jpyIndex]), float(split[gbpIndex])
+		r = float(split[usdIndex])
 		rates.append(r)
 
 	f.close()
@@ -26,4 +20,24 @@ def parseEuroXRef(file):
 	return rates
 
 rates = parseEuroXRef('trainingdata/eurofxref-hist.csv')
-print rates
+print len(rates)
+
+from pybrain.tools.shortcuts import buildNetwork
+
+TRAIN_KERNEL = 10
+TRAIN_STEPS = 20
+
+net = buildNetwork(TRAIN_KERNEL, 3, 1)
+
+from pybrain.datasets import SupervisedDataSet
+ds = SupervisedDataSet(TRAIN_KERNEL, 1)
+ds.addSample((0,0,0,0,0,0,0,0,0,0),(0))
+
+for i in  xrange(TRAIN_STEPS):
+	ds.addSample(tuple(rates[i:i+TRAIN_KERNEL]), (rates[i + TRAIN_KERNEL]))
+
+from pybrain.supervised.trainers import BackpropTrainer
+
+trainer = BackpropTrainer(net, ds)
+trainer.train()
+print trainer
